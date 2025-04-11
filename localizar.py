527338dd7ef94bc4eed2937fd1538d5746eb5c5d
gomesn1030 <gomesn1030@gmail.com>
2025-04-11 12:44:00 -0300

    ajsute para cidades

--- localizar.py
+++ localizar.py
@@ -1,16 +1,20 @@
 import streamlit as st
 import sqlite3
 
-def buscar_usos(tipo_uso):
-    conn = sqlite3.connect('zoneamento.db')
+# Funções auxiliares
+def conectar():
+    return sqlite3.connect('zoneamento.db')
+
+def buscar_usos(tipo_uso, cidade_id):
+    conn = conectar()
     cursor = conn.cursor()
 
     query = '''
         SELECT DISTINCT macrozona_codigo, zona_codigo
         FROM usos_permitidos
-        WHERE tipo_uso LIKE ? AND permissao = 'Permitido'
+        WHERE tipo_uso LIKE ? AND permissao = 'Permitido' AND cidade_id = ?
     '''
-    cursor.execute(query, (f"%{tipo_uso}%",))  # << aqui era o problema!!
+    cursor.execute(query, (f"%{tipo_uso}%", cidade_id))
     resultados = cursor.fetchall()
 
     conn.close()
@@ -19,11 +23,18 @@ def buscar_usos(tipo_uso):
 def pesquisar_uso():
     st.header("Pesquisar Uso Permitido")
 
+    conn = conectar()
+    cidades = conn.execute("SELECT * FROM cidade").fetchall()
+    conn.close()
+
+    cidade_nome = st.selectbox("Escolha a cidade:", [c[1] for c in cidades])
+    cidade_id = [c[0] for c in cidades if c[1] == cidade_nome][0]
+
     tipo_uso_input = st.text_input("Digite o tipo de uso para pesquisar:")
 
     if st.button("Buscar"):
         if tipo_uso_input:
-            resultados = buscar_usos(tipo_uso_input)  # Passa o input para a função
+            resultados = buscar_usos(tipo_uso_input, cidade_id)
             if resultados:
                 st.success(f"Encontramos {len(resultados)} resultado(s):")
                 for macrozona, zona in resultados:
